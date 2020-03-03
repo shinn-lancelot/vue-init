772d51190ab6a8445cd0f0c5dd4fa987a4f89784
shinn_lancelot <lanhaichun0317@gmail.com>
2020-03-03 08:55:08 +0800

    1. 修改app.vue为小写

---
+++ src/pages/main/app.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="app">
+		<avatar></avatar>
+		<div class="name">{{ name }}</div>
+		<div class="nav">
+			<router-link to="/">home</router-link> |
+			<router-link to="/hello">hello</router-link>
+		</div>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	import Avatar from 'components/Avatar'
+
+	export default {
+		name: 'App',
+		components: {
+			Avatar
+		},
+		props: {},
+		data () {
+			return {
+				name: 'shinn_lancelot'
+			}
+		},
+		watch: {},
+		computed: {},
+		beforeCreate () {},
+		created () {},
+		beforeMount () {},
+		mounted () {},
+		beforeUpdate() {},
+		updated () {},
+		beforeDestroy () {},
+		destroyed () {},
+		methods: {}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import "~common/scss/variables";
+
+	.app {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: flex-start;
+		align-items: center;
+		padding-top: $distance-medium * 5;
+		box-sizing: border-box;
+		.name {
+			color: $color-default;
+			box-sizing: border-box;
+			margin-top: $distance-medium;
+			color: $color-theme-dark;
+			font-size: $font-size-x-large;
+		}
+		.nav {
+			color: $color-default;
+			box-sizing: border-box;
+			margin: $distance-xxx-large auto;
+			font-size: $font-size-medium;
+			a {
+				font-weight: $font-weight-ten;
+				color: $color-theme-dark;
+				&.router-link-exact-active {
+					color: $color-theme;
+				}
+			}
+		}
+	}
+</style>
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App'
+import App from './app'
 import router from 'router/main-router'
 import store from 'store/main-store'
 import FastClick from 'fastclick'
