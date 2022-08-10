12ca5e8c01471a8504ed8dd70eb3cd74612025d7
shinn_lancelot <lanhaichun0317@gmail.com>
2022-08-10 17:03:37 +0800

    feat: 新增env配置

--- build/vite.config.js
+++ build/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig((conmand, mode) => {
+export default defineConfig((config) => {
+  let env = loadEnv(config.mode, process.cwd())
   // 选项：https://cn.vitejs.dev/config/shared-options.html
   return {
-    root: './src',
     plugins: [
       vue()
     ],
