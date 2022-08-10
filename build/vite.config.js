7a2f8faf7327969f588145f8e569850ab5dfa846
shinn_lancelot <lanhaichun0317@gmail.com>
2022-08-10 17:31:07 +0800

    feat: 新增alias配置

--- build/vite.config.js
+++ build/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig((config) => {
   let env = loadEnv(config.mode, process.cwd())
@@ -12,6 +13,18 @@ export default defineConfig((config) => {
       host: '0.0.0.0',
       port: 1180,
       open: '/index.html'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '../src'),
+        api: path.resolve(__dirname, '../src/api'),
+        assets: path.resolve(__dirname, '../src/assets'),
+        common: path.resolve(__dirname, '../src/common'),
+        components: path.resolve(__dirname, '../src/components'),
+        views: path.resolve(__dirname, '../src/views'),
+        router: path.resolve(__dirname, '../src/router'),
+        store: path.resolve(__dirname, '../src/store')
+      }
     }
   }
 })
