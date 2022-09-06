a56bc83f97e73e85006a8da78472920b8f41390d
shinn_lancelot <lanhaichun0317@gmail.com>
2022-09-06 16:43:39 +0800

    chore: 修改alias

--- vite.config.js
+++ vite.config.js
@@ -60,7 +60,7 @@ export default defineConfig((config) => {
         'assets': path.resolve(__dirname, 'src/assets'),
         'common': path.resolve(__dirname, 'src/common'),
         'components': path.resolve(__dirname, 'src/components'),
-        'views': path.resolve(__dirname, 'src/views'),
+        'pages': path.resolve(__dirname, 'src/pages'),
         'router': path.resolve(__dirname, 'src/router'),
         'store': path.resolve(__dirname, 'src/store'),
       },
