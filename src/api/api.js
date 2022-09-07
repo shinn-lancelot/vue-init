4aa6dccb385c35713e2c0c96b24799fd75af82a7
shinn_lancelot <lanhaichun0317@gmail.com>
2022-09-07 11:00:05 +0800

    refactor: 完善示例接口

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,12 @@ import http from 'common/js/http'
 
 // get接口请求示例
 const getExample = (data) => {
-  return http.get('/example', data)
+  return http.get('/get', data)
 }
 
 // post接口请求示例
 const postExample = (data) => {
-  return http.post('/example', data)
+  return http.post('/post', data)
 }
 
 export {
