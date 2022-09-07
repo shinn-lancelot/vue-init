787ac13a664435ee70d18238b9045efb75cbd646
shinn_lancelot <lanhaichun0317@gmail.com>
2022-09-07 08:50:55 +0800

    refactor: 新增App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import favicon from 'assets/images/favicon.ico'
+
+useHead({
+  title: import.meta.env.VITE_APP_TITLE,
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no' },
+    { name: 'description', content: 'a vue app' },
+  ],
+  link: [
+    { rel: 'shortcut icon', href: favicon },
+  ],
+})
+</script>
+
+<template>
+  <div>
+    This is a boilerplate project for vue.
+  </div>
+</template>
